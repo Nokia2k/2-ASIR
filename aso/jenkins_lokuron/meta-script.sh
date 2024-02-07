db3ad9d83f44605e3a1ea0ec2f7e4647b54ee1b8
Nokia <vicmarmartinezmartinez@gmail.com>
2024-02-07 12:09:35 +0100

    auto-script

---
+++ aso/jenkins_lokuron/meta-script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# DO NOT EDIT THIS SCRIPT 
+# IT WILL BE AUTOMAGICALLY GENERATED
+
+# Orig users-240122.xlsx
+# Dest users-240123.xlsx
+
+
+# DESPIDOS PROCEDENTES :  
+
+# Deleting Cesar Tomas
+deluser csueca
+
+# Deleting Angel Diseño
+deluser aberlanas
+
+
+# NUEVOS CONTRATOS 
+useradd -m -d "/home/jguesseado" -s "/bin/bash" -u 5004 -c "Jose Guesser, ,123554422, ,timeguesser@champion.co.uk" "jguesseado"
+echo "jguesseado:123554422"| chpasswd 
+
+# MODIFICACIONES DE LOS EMPLEADOS: 
+
+# apepino ha cambiado :
+# EL NOMBRE:
+chfn -f "Adrian Largo" apepino
+
+# lautarino ha cambiado :
+# EL LOGIN:
+usermod -l lautarin lautarino
+# EL NOMBRE:
+chfn -f "Lautaro Guapeton" lautarino
+# EL TELEFONO:
+chfn -r "123123333" lautarino
+# EL CORREO:
+usermod -c "terrorismo@argentina.ar" lautarino
+
+# vthin ha cambiado :
+# EL NOMBRE:
+chfn -f "Viktor Thonk" vthin
+
+# jmarchante ha cambiado :
+# EL TELEFONO:
+chfn -r "445543267" jmarchante
+exit 0
